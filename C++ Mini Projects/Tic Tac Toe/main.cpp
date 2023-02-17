393cf36ba1d76c13c331884101342a1cbfcde69e
Utsav Raj <utsavraj@Utsavs-MacBook-Air.local>
2023-02-17 12:12:55 +0530

    Tic Tac Toe single player completed

--- C++ Mini Projects/Tic Tac Toe/main.cpp
+++ C++ Mini Projects/Tic Tac Toe/main.cpp
@@ -19,28 +19,33 @@ class Game {
     Game(string player1, string player2) {
         arrayOfBoard = new int[9];
         for(int i = 0; i < 9; ++i) 
-            arrayOfBoard[i] = -1;
+            arrayOfBoard[i] = -1*(i+1);
         this->player1 = player1;
         this->player2 = player2;
     }
 
     char getSignChar(int k) {
         if(arrayOfBoard[k-1] == 0) return 'o';
-        else if(arrayOfBoard[-1] == 1) return 'x';
+        else if(arrayOfBoard[k-1] == 1) return 'x';
         return k + 48;
     }
 
     string status() {
-        // check diagonals
-        //for(int i = 0; i < ) 
         for(int i = 0; i < 3; i+=3)
             if(arrayOfBoard[i] == arrayOfBoard[i+1] && arrayOfBoard[i+1] == arrayOfBoard[i+2])
                 return (arrayOfBoard[i] == 'o')?"PLAYER1":"PLAYER2";
         for(int i = 0; i < 3; ++i)
-                if(arrayOfBoard[i] == arrayOfBoard[i+3] && arrayOfBoard[i+3] == arrayOfBoard[i+6])
-                    return (arrayOfBoard[i] == '0')?"PLAYER1":"PLAYER2";
-            
-
+            if(arrayOfBoard[i] == arrayOfBoard[i+3] && arrayOfBoard[i+3] == arrayOfBoard[i+6])
+                return (arrayOfBoard[i] == 'o')?"PLAYER1":"PLAYER2";
+        if(arrayOfBoard[0] == arrayOfBoard[4] && arrayOfBoard[4] == arrayOfBoard[8])
+            return (arrayOfBoard[4] == 'o')?"PLAYER1":"PLAYER2";
+        if(arrayOfBoard[2] == arrayOfBoard[4] && arrayOfBoard[4] == arrayOfBoard[6])
+            return (arrayOfBoard[4] == 'o')?"PLAYER1":"PLAYER2";
+        for(int i = 0; i < 9; ++i) {
+            if(arrayOfBoard[i] < 0) 
+                return "CONTINUE";
+        }
+        return "DRAW";
     }
 
     void drawBoard() {
@@ -61,12 +66,73 @@ class Game {
             }
         }
     }
+
+    inline void markPosition(int position, int player) {
+        arrayOfBoard[position-1] = player;
+    }
+
+    bool isValidPosition(int position) {
+        if(arrayOfBoard[position-1] == (-1 * position))
+            return true;
+        return false;
+    }
+
+    string play() {
+        string game_status = "CONTINUE";
+        int current_player = 0;
+        while(game_status == "CONTINUE") {
+            CLEAR;
+            this->drawBoard();
+            int position;
+            while(true) {
+                cout << "Choose a position : ";
+                cin >> position;
+                if(isValidPosition(position)) 
+                    break;
+                cout << "Invalid Position" << endl;
+            }
+            markPosition(position, current_player);
+
+            current_player++;
+            current_player %= 2;
+            game_status = this->status();
+        } 
+        current_player--;
+        if(game_status == "PLAYER1")
+            return this->player1;
+        else if(game_status == "PLAYER2")
+            return this->player2;
+        return "DRAW";
+    }
 };
 
 int main() {
-    Game newGame("A","B");
-    while(newGame.status() == "CONTINUE") {
-        newGame.drawBoard();
-    }
-    
+    while(true) {
+        string winner, player1, player2;
+        cout << "Enter Player1 Name: " << endl;
+        getline(cin, player1);
+        cout << "Enter player2 Name: " << endl;
+        getline(cin, player2);
+        Game newGame(player1, player2);
+        winner = newGame.play();
+        CLEAR;
+        if(winner == "DRAW") {
+            cout << "Draw" << endl;
+        }
+        else {
+            cout << winner << " Won!" << endl;
+        }
+        char answer;
+        do {
+            cout << "Do you want to play again : (Y/N) " << endl;
+            cin.clear();
+            cin >> answer;
+            if(answer != 'Y' && answer != 'N') {
+                cout << "Incorrect input" << endl;
+            }
+        }
+        while (answer != 'Y' && answer != 'N');
+        if(answer == 'N')
+            break;
+    }    
 }
